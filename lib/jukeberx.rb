38dfcd9acbb44110c0e0d084fa21c7b51acec0c4
Brit Butler <redline6561@gmail.com>
2015-06-01 11:41:28 -0400

    Get a really basic app written.

--- lib/jukeberx.rb
+++ lib/jukeberx.rb
@@ -1,5 +1,49 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require "json"
+require "pry"
+require "faker"
+require "sinatra"
+
 require "jukeberx/version"
 
 module Jukeberx
-  # Your code goes here...
+  STATUSES = {
+    'brit'     => 'feeling less sickly',
+    'randy'    => 'excited about apps',
+    'rickard'  => 'thumb twiddling',
+    'philip'   => 'lost and scared',
+    'kevin'    => 'Lost In New York',
+    'sherri'   => 'reading docs, solving problems'
+  }
+
+  def status(username)
+    STATUSES[username]
+  end
+
+  def random_status(username)
+    new_status = Faker::Hacker.say_something_smart
+    STATUSES[username] = new_status
+  end
 end
+
+helpers Jukeberx
+
+get '/status/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  username = params['name']
+  {
+    username: username,
+    status: status(username)
+  }.to_json
+end
+
+get '/random_status/:name' do
+  random_status(params['name'])
+  "Your status has been updated #{params['name']}!"
+end
+
+#get '/set_status/:name' do
+#
+#end

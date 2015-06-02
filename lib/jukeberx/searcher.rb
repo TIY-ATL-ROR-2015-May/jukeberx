6b4163344297f461a2f7c2fd0eec65f93b2e8699
Brit Butler <redline6561@gmail.com>
2015-06-02 10:40:58 -0400

    Flesh out Searcher class and implement Enumerable!

--- lib/jukeberx/searcher.rb
+++ lib/jukeberx/searcher.rb
@@ -1,11 +1,21 @@
 module Jukeberx
   class Searcher
+    include Enumerable
+
     def initialize(dir)
-      @search_path = dir
+      @songs = self.get_mp3s(dir)
     end
 
-    def get_mp3s
-      binding.pry
+    def each
+      @songs.each { |x| yield(x) }
+    end
+
+    def get_mp3s(dir)
+      files = Dir.glob(File.join(dir, '*/*.mp3'))
+      files.map do |mp3_file|
+        tag = Mp3Info.open(mp3_file) { |mp3| mp3.tag }
+        Song.new(mp3_file, tag)
+      end
     end
   end
 end

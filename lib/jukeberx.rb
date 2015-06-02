cd56edc01f1c7698de59bfad35519a7dbc80c9e4
Brit Butler <redline6561@gmail.com>
2015-06-02 09:44:03 -0400

    Add outline of searcher class.

--- lib/jukeberx.rb
+++ lib/jukeberx.rb
@@ -3,8 +3,13 @@ require "json"
 require "faker"
 require "sinatra"
 
-require "jukeberx/version"
+require 'jukeberx/version'
+require 'jukeberx/searcher'
+
+MUSIC_DIR = '/Users/brit/Music/downloads'
 
 module Jukeberx
-  
 end
+
+searcher = Jukeberx::Searcher.new(MUSIC_DIR)
+searcher.get_mp3s
---
+++ lib/jukeberx/searcher.rb
@@ -0,0 +1,11 @@
+module Jukeberx
+  class Searcher
+    def initialize(dir)
+      @search_path = dir
+    end
+
+    def get_mp3s
+      binding.pry
+    end
+  end
+end

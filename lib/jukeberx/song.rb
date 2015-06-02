1a1c79f8707e2e0a95c4851d5668dc0e46b6d56c
Brit Butler <redline6561@gmail.com>
2015-06-02 11:55:04 -0400

    Songs have an id and we can play them!

--- lib/jukeberx/song.rb
+++ lib/jukeberx/song.rb
@@ -1,12 +1,18 @@
 module Jukeberx
   class Song
-    attr_reader :filename, :artist, :album, :title
+    attr_reader :id, :filename, :artist, :album, :title
 
-    def initialize(file, tag)
+    def initialize(id, file, tag)
+      @pid = nil
+      @id = id
       @filename = file
       @artist = tag.artist
       @album = tag.album
       @title = tag.title
+    end
+
+    def play
+      @pid = spawn("afplay #{@filename}")
     end
   end
 end

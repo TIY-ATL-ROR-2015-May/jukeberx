ccbef523c633dd7c5c1e00352e61508fd4294f5a
Brit Butler <redline6561@gmail.com>
2015-06-02 11:55:27 -0400

    Start on a basic server.

--- lib/jukeberx.rb
+++ lib/jukeberx.rb
@@ -1,7 +1,7 @@
 require "pry"
 require "json"
 require "mp3info"
-require "sinatra"
+require "sinatra/base"
 
 require 'jukeberx/version'
 require 'jukeberx/song'
@@ -10,9 +10,34 @@ require 'jukeberx/searcher'
 MUSIC_DIR = '/Users/brit/Music/downloads'
 
 module Jukeberx
+  class App < Sinatra::Base
+    set :library, Jukeberx::Searcher.new(MUSIC_DIR)
+    enable :logging
+
+    get '/' do
+      "Do you know how to use the API? Try these routes!
+            '/artists',
+            '/albums',
+            '/titles'"
+    end
+
+    get '/artists' do
+      settings.library.list_artists.to_json
+    end
+
+    get '/albums' do
+      settings.library.list_albums.to_json
+    end
+
+    get '/titles' do
+      settings.library.list_titles.to_json
+    end
+
+    post '/play' do
+      
+    end
+
+    run! if app_file == $0
+  end
 end
 
-searcher = Jukeberx::Searcher.new(MUSIC_DIR)
-searcher.get_mp3s
-
-binding.pry

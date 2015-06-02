0f52a5fd9c9c804f8857adce5033ea78c353fae9
Brit Butler <redline6561@gmail.com>
2015-06-02 11:56:13 -0400

    Fill in the song IDs in the Searcher and handle errors.

--- lib/jukeberx/searcher.rb
+++ lib/jukeberx/searcher.rb
@@ -3,7 +3,8 @@ module Jukeberx
     include Enumerable
 
     def initialize(dir)
-      @songs = self.get_mp3s(dir)
+      @songs = []
+      self.get_mp3s(dir)
     end
 
     def each
@@ -12,9 +13,13 @@ module Jukeberx
 
     def get_mp3s(dir)
       files = Dir.glob(File.join(dir, '*/*.mp3'))
-      files.map do |mp3_file|
-        tag = Mp3Info.open(mp3_file) { |mp3| mp3.tag }
-        Song.new(mp3_file, tag)
+      files.each_with_index do |mp3_file, id|
+        begin
+          tag = Mp3Info.open(mp3_file) { |mp3| mp3.tag }
+          @songs << Song.new(id, mp3_file, tag)
+        rescue Mp3InfoError => e
+          puts "#{mp3_file} failed with: #{e.message}"
+        end
       end
     end
   end
